0973788e9339fc2695039ce43cc527b34356d460
אריאל רוטשטין <ariel33856@gmail.com>
2025-12-27 19:48:12 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -135,12 +135,12 @@ export default function Dashboard() {
                 <Table className="w-4 h-4" />
               </Button>
             </div>
-            </div>
-            </div>
-            </div>
+          </div>
+        </div>
+      </div>
 
-            <div className="mx-auto p-1">
-            {/* Cases Content */}
+      <div className="mx-auto p-1">
+        {/* Cases Content */}
         {isLoading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {[1, 2, 3, 4, 5, 6].map(i => (
