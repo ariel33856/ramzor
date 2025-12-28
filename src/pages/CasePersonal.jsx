f6a07aeac7deb00b13817d5ef5d52e49c02fd2c8
אריאל רוטשטין <ariel33856@gmail.com>
2025-12-28 21:48:32 +0000

    File changes

--- src/pages/CasePersonal.jsx
+++ src/pages/CasePersonal.jsx
@@ -263,8 +263,8 @@ export default function CasePersonal() {
   }
 
   return (
-    <div className="mx-auto">
-      <div className="flex justify-end gap-2 mb-4">
+    <>
+      <div className="flex justify-end gap-2 mb-4 px-2">
           {linkedBorrowers.length > 0 && linkedBorrowers[0]?.person_id && (
             <Link to={createPageUrl('PersonDetails') + `?id=${linkedBorrowers[0].person_id}`}>
               <Button className="bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700">
