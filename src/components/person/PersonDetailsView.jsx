30cbcb5cc279a81c71d5b0810b7c557fc05cbd52
אריאל רוטשטין <ariel33856@gmail.com>
2025-12-29 23:55:32 +0000

    File changes

--- src/components/person/PersonDetailsView.jsx
+++ src/components/person/PersonDetailsView.jsx
@@ -663,10 +663,6 @@ export default function PersonDetailsView({ personId }) {
         </div>
 
         <div className="flex items-center gap-2">
-          <Label className="text-sm whitespace-nowrap">שדה 3</Label>
-          <Input />
-        </div>
-        <div className="flex items-center gap-2">
           <Label className="text-sm whitespace-nowrap">שדה 4</Label>
           <Input />
         </div>
