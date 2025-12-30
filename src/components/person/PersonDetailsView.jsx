c740361cb2d3d23f111efbbc0c8ca4215f1ad1ad
אריאל רוטשטין <ariel33856@gmail.com>
2025-12-30 02:08:57 +0000

    File changes

--- src/components/person/PersonDetailsView.jsx
+++ src/components/person/PersonDetailsView.jsx
@@ -826,10 +826,6 @@ export default function PersonDetailsView({ personId }) {
 
         <div></div>
         <div></div>
-        <div className="flex items-center gap-2">
-          <Label className="text-sm whitespace-nowrap">שדה 4</Label>
-          <Input />
-        </div>
       </div>
 
       {showChildrenWarning && (
