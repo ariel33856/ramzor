65c90ee93341fac1a3e45fa5e45da2dfd0d2c895
אריאל רוטשטין <ariel33856@gmail.com>
2026-01-10 19:53:09 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -506,7 +506,7 @@ export default function Dashboard() {
                         {field?.label || fieldId}
                       </button>
                     </PopoverTrigger>
-                    <PopoverContent className="w-40 p-2 mr-[50px]" align="start">
+                    <PopoverContent className="w-40 p-2 mr-[100px]" align="start">
                       <div className="space-y-1">
 
                         <Button
@@ -540,8 +540,8 @@ export default function Dashboard() {
                         )}
                       </button>
                     </PopoverTrigger>
-                    <PopoverContent className="w-80 mr-[100px]" align="start">
-                      <div className="space-y-3">
+                    <PopoverContent className="w-80 mr-[500px]" align="start">
+                            <div className="space-y-3">
                         <h4 className="font-semibold text-sm">
                           סנן לפי {field?.label || fieldId}
                         </h4>
