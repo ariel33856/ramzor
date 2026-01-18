7678501e5622aa29e11a4f61e900fb7f3eadc867
אריאל רוטשטין <ariel33856@gmail.com>
2026-01-18 23:38:55 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -447,7 +447,7 @@ export default function Dashboard() {
 
               <Popover open={reorderDialogOpen} onOpenChange={setReorderDialogOpen}>
                 <PopoverTrigger asChild>
-                  <Button variant="outline" className="mr-auto">
+                  <Button variant="outline" className="ml-4">
                     <GripVertical className="w-5 h-5 ml-2" />
                     סדר עמודות
                   </Button>
