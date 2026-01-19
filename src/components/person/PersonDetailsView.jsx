72fa0d6610c219b279ababcd3781357842bf47d5
אריאל רוטשטין <ariel33856@gmail.com>
2026-01-19 00:54:35 +0000

    File changes

--- src/components/person/PersonDetailsView.jsx
+++ src/components/person/PersonDetailsView.jsx
@@ -1102,7 +1102,7 @@ export default function PersonDetailsView({ personId }) {
                   custom_data: { ...(person?.custom_data || {}), income_source: value }
                 });
               }}>
-                <SelectTrigger className="w-64">
+                <SelectTrigger className="w-32">
                   <SelectValue placeholder="בחר מקור הכנסה" />
                 </SelectTrigger>
                 <SelectContent>
