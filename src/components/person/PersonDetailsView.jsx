3814b76aa2f48bd4d4f409ef73da11c7dfe0ace7
אריאל רוטשטין <ariel33856@gmail.com>
2026-01-19 13:15:44 +0000

    File changes

--- src/components/person/PersonDetailsView.jsx
+++ src/components/person/PersonDetailsView.jsx
@@ -1248,7 +1248,6 @@ export default function PersonDetailsView({ personId }) {
               </div>
               </div>
               </div>
-              </div>
               )}
               </div>
               </div>
