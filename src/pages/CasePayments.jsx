2afaa53f50522fbc0b7d3932f37bfb16a3cff181
אריאל רוטשטין <ariel33856@gmail.com>
2026-01-24 23:55:04 +0000

    File changes

--- src/pages/CasePayments.jsx
+++ src/pages/CasePayments.jsx
@@ -120,7 +120,7 @@ export default function CasePayments() {
             <Input
               type="text"
               inputMode="decimal"
-              value={new Intl.NumberFormat('he-IL').format(editValues[fieldName] !== undefined ? editValues[fieldName] : priceWithoutVat)}
+              value={editValues[fieldName] !== undefined && editValues[fieldName] ? new Intl.NumberFormat('he-IL').format(editValues[fieldName]) : '₪'}
               onChange={(e) => {
                 const numValue = e.target.value.replace(/,/g, '');
                 if (!isNaN(numValue) || numValue === '') {
