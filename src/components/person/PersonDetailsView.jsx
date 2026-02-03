60fb783a29341b9e3259d3adbf7e66ea8f0e08ff
אריאל רוטשטין <ariel33856@gmail.com>
2026-02-03 20:35:49 +0000

    File changes

--- src/components/person/PersonDetailsView.jsx
+++ src/components/person/PersonDetailsView.jsx
@@ -746,8 +746,7 @@ export default function PersonDetailsView({ personId }) {
               </div>
               );
               })}
-              </>
-              ) : (
+            ) : (
             <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
               <DialogTrigger asChild>
                 <Button className="bg-gradient-to-r from-green-600 to-emerald-600 hover:from-green-700 hover:to-emerald-700 whitespace-nowrap">
