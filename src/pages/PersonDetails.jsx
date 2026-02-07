8fefa5a34d384b0cc5b739b75b3310d53011282a
אריאל רוטשטין <ariel33856@gmail.com>
2026-02-07 20:55:48 +0000

    File changes

--- src/pages/PersonDetails.jsx
+++ src/pages/PersonDetails.jsx
@@ -6,10 +6,8 @@ export default function PersonDetails() {
   const personId = urlParams.get('id');
 
   return (
-    <div className="h-full bg-gray-50/50 p-6">
-      <div className="max-w-full mx-auto">
-        <PersonDetailsView personId={personId} />
-      </div>
+    <div className="h-full bg-gray-50/50">
+      <PersonDetailsView personId={personId} />
     </div>
   );
 }
