e61003ea0125b2d46cba94c24ff0871a9841bc5d
אריאל רוטשטין <ariel33856@gmail.com>
2026-02-08 20:21:26 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -61,7 +61,6 @@ import CaseContact from './pages/CaseContact';
 import CaseContacts from './pages/CaseContacts';
 import CaseDashboards from './pages/CaseDashboards';
 import CaseData from './pages/CaseData';
-import CaseDetails from './pages/CaseDetails';
 import CaseDocuments from './pages/CaseDocuments';
 import CaseInsurance from './pages/CaseInsurance';
 import CaseMetrics from './pages/CaseMetrics';
@@ -98,6 +97,7 @@ import RecordsTable from './pages/RecordsTable';
 import Sales from './pages/Sales';
 import SearchPage from './pages/SearchPage';
 import Transactions from './pages/Transactions';
+import CaseDetails from './pages/CaseDetails';
 import __Layout from './Layout.jsx';
 
 
@@ -116,7 +116,6 @@ export const PAGES = {
     "CaseContacts": CaseContacts,
     "CaseDashboards": CaseDashboards,
     "CaseData": CaseData,
-    "CaseDetails": CaseDetails,
     "CaseDocuments": CaseDocuments,
     "CaseInsurance": CaseInsurance,
     "CaseMetrics": CaseMetrics,
@@ -153,6 +152,7 @@ export const PAGES = {
     "Sales": Sales,
     "SearchPage": SearchPage,
     "Transactions": Transactions,
+    "CaseDetails": CaseDetails,
 }
 
 export const pagesConfig = {
