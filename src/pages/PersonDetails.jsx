a2fbf7d585af76471f488c04fe6aad4a0db9edf8
אריאל רוטשטין <ariel33856@gmail.com>
2026-02-08 19:06:40 +0000

    File changes

--- src/pages/PersonDetails.jsx
+++ src/pages/PersonDetails.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PersonDetailsView from '@/components/person/PersonDetailsView';
 
 export default function PersonDetails() {
   const urlParams = new URLSearchParams(window.location.search);
   const personId = urlParams.get('id');
+
+  useEffect(() => {
+    if (personId) {
+      localStorage.setItem('lastOpenedPersonId', personId);
+    }
+  }, [personId]);
 
   return (
     <div className="h-full">
